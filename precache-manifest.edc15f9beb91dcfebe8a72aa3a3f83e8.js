2ef187a4aa9cfe48439d426c2d70fef3fde3e925
Daniel Saewitz <switz213@gmail.com>
2019-04-15 06:33:25 -0400

    Updates

---
+++ precache-manifest.edc15f9beb91dcfebe8a72aa3a3f83e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ce46700d623cc48f3e4",
+    "url": "/sotw/static/css/main.b09a5c15.chunk.css"
+  },
+  {
+    "revision": "7ce46700d623cc48f3e4",
+    "url": "/sotw/static/js/main.a5beda86.chunk.js"
+  },
+  {
+    "revision": "b4370f8e46e9d1aaac12",
+    "url": "/sotw/static/js/runtime~main.bbf9da09.js"
+  },
+  {
+    "revision": "824cd9a240b4f827e2cc",
+    "url": "/sotw/static/js/2.e30e8ab5.chunk.js"
+  },
+  {
+    "revision": "5b049b8c77f262bd55f34a356b92d6ab",
+    "url": "/sotw/index.html"
+  }
+];
