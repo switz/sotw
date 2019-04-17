77350527f020f886347fa75aa26305751f11e74a
Daniel Saewitz <switz213@gmail.com>
2019-04-17 16:34:08 -0400

    Updates

---
+++ precache-manifest.43910b15396fa051bfd038a5ce1bb288.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a1478fcfadbbfe2354f",
+    "url": "/sotw/static/css/main.fa32b329.chunk.css"
+  },
+  {
+    "revision": "5a1478fcfadbbfe2354f",
+    "url": "/sotw/static/js/main.b54976f6.chunk.js"
+  },
+  {
+    "revision": "b4370f8e46e9d1aaac12",
+    "url": "/sotw/static/js/runtime~main.bbf9da09.js"
+  },
+  {
+    "revision": "a24db72de9db4881852c",
+    "url": "/sotw/static/css/2.21e2cd80.chunk.css"
+  },
+  {
+    "revision": "a24db72de9db4881852c",
+    "url": "/sotw/static/js/2.d5d0906f.chunk.js"
+  },
+  {
+    "revision": "b6382c6d9ffca4a1c50f58a17fce15a9",
+    "url": "/sotw/static/media/icons-16.b6382c6d.ttf"
+  },
+  {
+    "revision": "3326c360137c2d6a6909d0c4303e502a",
+    "url": "/sotw/static/media/icons-16.3326c360.eot"
+  },
+  {
+    "revision": "895bc7de59413bb8029ac83cac8c9e1b",
+    "url": "/sotw/static/media/icons-16.895bc7de.woff"
+  },
+  {
+    "revision": "058e48cbbee6cdbfb2a2e6dab347f245",
+    "url": "/sotw/static/media/icons-20.058e48cb.woff"
+  },
+  {
+    "revision": "e666d692a7dc07ea69886dfcec837c3c",
+    "url": "/sotw/static/media/icons-20.e666d692.eot"
+  },
+  {
+    "revision": "aa8f3ca044cd237f87b69d0cb364887a",
+    "url": "/sotw/static/media/icons-20.aa8f3ca0.ttf"
+  },
+  {
+    "revision": "9d0f0f96522b079a022842313b1eea61",
+    "url": "/sotw/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sotw/precache-manifest.2dbdb8c7df91e8969e7a83ceb8d914ee.js"
+  "/sotw/precache-manifest.43910b15396fa051bfd038a5ce1bb288.js"
 );
 
 workbox.skipWaiting();
